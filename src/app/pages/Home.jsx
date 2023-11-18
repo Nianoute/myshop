2f41e013cfe2ac0389ec89dac30949bdb2a75b6e
nianoute <enzo.angot@gmail.com>
2023-11-18 19:49:23 +0100

    feat: home head

--- src/app/pages/Home.jsx
+++ src/app/pages/Home.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const HomePage = () => {
   return (
     <div className="home">
-      <h1>Home Page</h1>
+      <div className="space" />
+      <div className="head">
+        <div className="title">
+          <h1>CULTURE CAMPUS</h1>
+          <p>Découvrez notre nouvelle collection!</p>
+          <p>HÉRITAGE</p>
+          <div className="primary_button">Découvrir</div>
+        </div>
+      </div>
+      <div className="collection">
+        <h2>NOS COLLECTIONS</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into el
+        </p>
+        <div className="primary_button nomargin">Découvrir</div>
+      </div>
     </div>
   );
 };
